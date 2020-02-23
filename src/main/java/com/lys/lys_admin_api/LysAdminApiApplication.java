01c9bd4f6b910dc375e472f15ec03692be54701e
yunets <724547564@qq.com>
2020-02-23 18:30:42 +0800

    first commit

---
+++ src/main/java/com/lys/lys_admin_api/LysAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.lys.lys_admin_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LysAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LysAdminApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lys/lys_admin_api/LysAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lys.lys_admin_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LysAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
