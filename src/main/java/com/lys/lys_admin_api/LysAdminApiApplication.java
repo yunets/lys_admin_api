9b6bc555047ec478283a6a391756855f08e7e20f
yunets <724547564@qq.com>
2020-07-10 15:44:36 +0800

    modify

--- src/main/java/com/lys/lys_admin_api/LysAdminApiApplication.java
+++ src/main/java/com/lys/lys_admin_api/LysAdminApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.lys.lys_admin_api.module.demo.sys.mapper")
+@MapperScan("com.lys.lys_admin_api.module.sys.mapper")
 
 public class LysAdminApiApplication {
 
